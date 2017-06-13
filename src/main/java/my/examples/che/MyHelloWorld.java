c0aabcd20e018f99d9d6707462bca76fe1e4b962
AR <anu.raghu.z@gmail.com>
2017-06-13 12:55:56 +0530

    source code

---
+++ src/main/java/my/examples/che/MyHelloWorld.java
@@ -0,0 +1,11 @@
+package my.examples.che;
+
+public class MyHelloWorld {
+ public static void main(String... argvs) {
+        String a = argvs[0];
+        System.out.println("Hello World " + a + "!!");
+        
+    }    
+     
+}
+
